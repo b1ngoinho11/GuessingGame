955a121b42e29a510bc6f7fcb4ee2114309b5324
b1ngoinho11 <budthbundit.thayaurai@gmail.com>
2025-03-12 20:08:34 +0700

    setup react.js + tailwind + shadcn

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+import { Button } from "@/components/ui/button"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div class="text-xl">
+        dsaasdsad
+      </div>
+      <Button variant="default">Button</Button>
+    </>
+  )
+}
+
+export default App
